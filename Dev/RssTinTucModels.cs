1dd87377596b9b8d5a77d28fdf2f00da0d558931
haylam**** <guanglee16@gmail.com>
2016-12-23 22:57:44 +0700

    Comit
    sadsadsa

---
+++ Dev/RssTinTucModels.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ShopOnline.Models
+{
+    public class RSSItem
+    {
+        public string Title { get; set; }
+
+        public string Link { get; set; }
+
+        public string Description { get; set; }
+
+        public DateTime PubDate { get; set; }
+    }
+
+    
+}
