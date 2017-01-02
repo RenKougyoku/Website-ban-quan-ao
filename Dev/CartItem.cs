640d0e77601543c3ed865cf4d50ff2e1e9f0024f
tambinh <tambinh95@gmail.com>
2017-01-02 19:42:33 +0700

    Cart
    v1.0

---
+++ Dev/CartItem.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+
+using webmaylanh.Models;
+
+namespace Shoesws.ViewModels
+{
+    public class CartItem
+    {
+        //Properties
+        public SanPham SanPham { get; set; }
+        public int SanPhamID { get; set; }
+        public int SoLuong { get; set; }
+
+         //Constructors
+        public CartItem(int SoLuong, int SanPhamID)
+        {
+            //this.SanPham = SanPham;
+            this.SanPhamID = SanPhamID;
+            this.SoLuong = SoLuong;
+        }
+    }
+}
