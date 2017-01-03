a6b235a9e53a3d5bfe41055c434636d53876b768
RenKougyoku <chitam1333@gmail.com>
2017-01-03 16:52:20 +0700

    1 dong

---
+++ howtodriveright/app/src/main/java/com/driver/tam/howtodriveitright/ObjectLatLng.java
@@ -0,0 +1,31 @@
+package com.driver.tam.howtodriveitright;
+
+public class ObjectLatLng {
+
+    private double mLat;
+    private double mLng;
+
+    public ObjectLatLng(double mLat, double mLng) {
+        super();
+        this.setmLat(mLat);
+        this.setmLng(mLng);
+    }
+
+    //@@@
+    public double getmLat() {
+        return mLat;
+    }
+
+    public void setmLat(double mLat) {
+        this.mLat = mLat;
+    }
+
+    //@@@
+    public double getmLng() {
+        return mLng;
+    }
+
+    public void setmLng(double mLng) {
+        this.mLng = mLng;
+    }
+}
